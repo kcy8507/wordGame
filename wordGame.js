af2f4cd9803e1241f79dd391e234b910b711beba
kcy8507 <kcy85078507@gmail.com>
2021-05-11 17:48:46 +0900

    Update wordGame.js

--- wordGame.js
+++ wordGame.js
@@ -1,6 +1,7 @@
-let word = "채연";
-while (true) {
-  let newWord = prompt("word");
+// let word = "채연";
+// while (true) {
+for (let word = "채연"; true; ) {
+  let newWord = prompt(word);
   if (word[word.length - 1] === newWord[0]) {
     word = newWord;
   } else {
